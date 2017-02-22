0ceabc1c2cceea4be197d27f0f1625acf8093fd8
vthglyk <vasgl@intracom-telecom.com>
2017-02-22 11:22:19 +0200

    Changed CloudResource

--- src/main/java/eu/h2020/symbiote/cloud/model/CloudResource.java
+++ src/main/java/eu/h2020/symbiote/cloud/model/CloudResource.java
@@ -1,11 +1,25 @@
 package eu.h2020.symbiote.cloud.model;
 
-import eu.h2020.symbiote.core.model.Resource;
+import org.springframework.data.annotation.Id;
 
+import java.util.List;
+import eu.h2020.symbiote.core.model.Location;
 
-public class CloudResource extends Resource {
-
+public class CloudResource {
+    @Id
     private String internalId;
+    private String id;
+    private String name;
+    private String owner;
+    private String description;
+    private List<String> observedProperties;
+    private String resourceURL;
+    private Location location;
+    private String featureOfInterest = null;
+    private String platformId;
+
+    public CloudResource() {
+    }
 
     public String getInternalId() {
         return internalId;
@@ -15,4 +29,75 @@ public class CloudResource extends Resource {
         this.internalId = internalId;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getObservedProperties() {
+        return observedProperties;
+    }
+
+    public void setObservedProperties(List<String> observedProperties) {
+        this.observedProperties = observedProperties;
+    }
+
+    public String getResourceURL() {
+        return resourceURL;
+    }
+
+    public void setResourceURL(String resourceURL) {
+        this.resourceURL = resourceURL;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getFeatureOfInterest() {
+        return featureOfInterest;
+    }
+
+    public void setFeatureOfInterest(String featureOfInterest) {
+        this.featureOfInterest = featureOfInterest;
+    }
+
+    public String getPlatformId() {
+        return platformId;
+    }
+
+    public void setPlatformId(String platformId) {
+        this.platformId = platformId;
+    }
 }
