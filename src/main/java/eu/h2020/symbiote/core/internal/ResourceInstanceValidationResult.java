411e783830fd342dd1d8bdd688ac921dc5f4da98
Szymon Mueller <szymon.mueller@man.poznan.pl>
2017-04-06 14:41:33 +0200

    Removing ResourceDescription class (since its the same as CoreResource) and changing related validation result class

--- src/main/java/eu/h2020/symbiote/core/internal/ResourceInstanceValidationResult.java
+++ src/main/java/eu/h2020/symbiote/core/internal/ResourceInstanceValidationResult.java
@@ -1,5 +1,7 @@
 package eu.h2020.symbiote.core.internal;
 
+import eu.h2020.symbiote.core.model.internal.CoreResource;
+
 import java.util.List;
 
 /**
@@ -7,13 +9,13 @@ import java.util.List;
  *
  * Created by Mael on 16/03/2017.
  */
-public class ResourceInstanceValidationResult extends ValidationResult<List<ResourceDescription>> {
+public class ResourceInstanceValidationResult extends ValidationResult<List<CoreResource>> {
 
     public ResourceInstanceValidationResult() {
 
     }
 
-    public ResourceInstanceValidationResult(boolean success, String description, String modelToBeValidated, String modelValidatedAgainst, List<ResourceDescription> objectDescription) {
+    public ResourceInstanceValidationResult(boolean success, String description, String modelToBeValidated, String modelValidatedAgainst, List<CoreResource> objectDescription) {
         super(success, description, modelToBeValidated, modelValidatedAgainst, objectDescription);
     }
 }
