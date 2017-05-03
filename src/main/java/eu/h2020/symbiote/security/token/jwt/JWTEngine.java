8c1615e2e58495dae91499a44505ee04794b7343
Mikolaj Dobski <mikolaj.dobski@man.poznan.pl>
2017-05-03 15:38:51 +0200

    switching some JWTEngine methods to static, fixing some exceptions

--- src/main/java/eu/h2020/symbiote/security/token/jwt/JWTEngine.java
+++ src/main/java/eu/h2020/symbiote/security/token/jwt/JWTEngine.java
@@ -3,7 +3,6 @@ package eu.h2020.symbiote.security.token.jwt;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.h2020.symbiote.security.commons.Constants;
-
 import eu.h2020.symbiote.security.commons.enums.IssuingAuthorityType;
 import eu.h2020.symbiote.security.commons.exceptions.JWTCreationException;
 import eu.h2020.symbiote.security.commons.exceptions.MalformedJWTException;
@@ -18,7 +17,6 @@ import org.apache.commons.logging.LogFactory;
 
 import java.io.IOException;
 import java.security.*;
-import java.security.cert.Certificate;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.*;
@@ -36,6 +34,67 @@ public class JWTEngine {
     private static Log log = LogFactory.getLog(JWTEngine.class);
 
     private SecureRandom random = new SecureRandom();
+
+    private static void validateToken(SymbIoTeToken token, PublicKey publicKey) throws TokenValidationException {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(publicKey)
+                    .parseClaimsJws(token.getToken()).getBody();
+            token.setClaims(claims);
+        } catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException | IllegalArgumentException e) {
+            throw new TokenValidationException("Token could not be validated", e);
+        }
+    }
+
+    public static JWTClaims getClaimsFromToken(String jwtToken) throws MalformedJWTException {
+
+        HashMap<String, Object> retMap = new HashMap<String, Object>();
+        String[] jwtParts = jwtToken.split("\\.");
+        if (jwtParts.length < Constants.JWTPartsCount) {
+            throw new MalformedJWTException();
+        }
+        //Get second part of the JWT
+        String jwtBody = jwtParts[1];
+
+        String claimsString = StringUtils.newStringUtf8(Base64.decodeBase64(jwtBody));
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        Map<String, Object> claimsMap = new HashMap<String, Object>();
+
+        try {
+            claimsMap = mapper.readValue(claimsString, new TypeReference<Map<String, String>>() {
+            });
+
+            Map<String, String> attributes = new HashMap<>();
+            Set<String> jwtKeys = claimsMap.keySet();
+            for (String key : jwtKeys) {
+                Object value = claimsMap.get(key);
+                if (key.startsWith(Constants.SYMBIOTE_ATTRIBUTES_PREFIX))
+                    attributes.put(key.substring(Constants.SYMBIOTE_ATTRIBUTES_PREFIX.length()), (String) value);
+                else
+                    retMap.put(key, value);
+            }
+
+            //Extracting claims from JWT claims map
+            JWTClaims retClaims = new JWTClaims(retMap.get("jti"), retMap.get("alg"), retMap.get("iss"), retMap.get("sub"), retMap
+                    .get("iat"), retMap.get("exp"), retMap.get("ipk"), retMap.get("spk"), retMap.get("ttyp"), attributes);
+
+            //Convert IPK claim to publicKey for validation
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.decodeBase64(retClaims.getIpk()));
+            KeyFactory keyFactory = KeyFactory.getInstance("EC");
+            PublicKey pubKey = keyFactory.generatePublic(keySpec);
+
+            //Validate token signature
+            validateToken(new SymbIoTeToken(jwtToken), pubKey);
+
+            return retClaims;
+
+        } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException | TokenValidationException e) {
+            log.error(e);
+            throw new MalformedJWTException(e);
+        }
+    }
 
     public String generateJWTToken(String userId, Map<String, String> attributes, byte[] userPublicKey, IssuingAuthorityType deploymentType, Long tokenValidity, String deploymentID, PublicKey aamPublicKey, PrivateKey aamPrivateKey) throws JWTCreationException {
 
@@ -84,74 +143,6 @@ public class JWTEngine {
             String message = "JWT creation error";
             log.error(message, e);
             throw new JWTCreationException(message, e);
-        }
-    }
-
-    private void validateToken(SymbIoTeToken token, PublicKey publicKey) throws TokenValidationException
-    {
-        try {
-            Claims claims = Jwts.parser()
-                    .setSigningKey(publicKey)
-                    .parseClaimsJws(token.getToken()).getBody();
-            token.setClaims(claims);
-        }catch(ExpiredJwtException| UnsupportedJwtException| MalformedJwtException| SignatureException | IllegalArgumentException e){
-            throw new TokenValidationException("Token could not be validated", e);
-        }
-    }
-
-    public JWTClaims getClaimsFromToken(String jwtToken) throws MalformedJWTException {
-
-        HashMap<String, Object> retMap = new HashMap<String, Object>();
-        String[] jwtParts = jwtToken.split("\\.");
-        if (jwtParts.length < Constants.JWTPartsCount) {
-            throw new MalformedJWTException();
-        }
-        //Get second part of the JWT
-        String jwtBody = jwtParts[1];
-
-        String claimsString = StringUtils.newStringUtf8(Base64.decodeBase64(jwtBody));
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        Map<String, Object> claimsMap = new HashMap<String, Object>();
-
-        try {
-            claimsMap = mapper.readValue(claimsString, new TypeReference<Map<String, String>>(){});
-
-            Map<String, String> attributes = new HashMap<>();
-            Set<String> jwtKeys = claimsMap.keySet();
-            Iterator<String> iter = jwtKeys.iterator();
-            while (iter.hasNext()) {
-                String key = iter.next();
-                Object value = claimsMap.get(key);
-                if (key.startsWith(Constants.SYMBIOTE_ATTRIBUTES_PREFIX))
-                    attributes.put(key.substring(Constants.SYMBIOTE_ATTRIBUTES_PREFIX.length()), (String) value);
-                else
-                    retMap.put(key, value);
-            }
-
-            //Extracting claims from JWT claims map
-            JWTClaims retClaims = new JWTClaims(retMap.get("jti"), retMap.get("alg"), retMap.get("iss"), retMap.get("sub"), retMap
-                    .get("iat"), retMap.get("exp"), retMap.get("ipk"), retMap.get("spk"), retMap.get("ttyp"), attributes);
-
-            //Convert IPK claim to publicKey for validation
-            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.decodeBase64(retClaims.getIpk()));
-            KeyFactory keyFactory = KeyFactory.getInstance("EC");
-            PublicKey pubKey = keyFactory.generatePublic(keySpec);
-
-            //Validate token signature
-            validateToken(new SymbIoTeToken(jwtToken),pubKey);
-
-            return retClaims;
-
-        } catch (IOException e) {
-            throw new MalformedJWTException();
-        } catch (NoSuchAlgorithmException e) {
-            throw new MalformedJWTException();
-        } catch (InvalidKeySpecException e) {
-            throw new MalformedJWTException();
-        } catch (TokenValidationException e) {
-            throw new MalformedJWTException();
         }
     }
 }
