713e4960b61abac0924bcd0b12240c080e76a929
petarkrivic <petarkrivic@10.19.4.102>
2017-05-04 11:54:32 +0200

    -added message model for platform proxy message to enabler logic.

---
+++ src/main/java/eu/h2020/symbiote/enabler/messaging/model/EnablerLogicDataAppearedMessage.java
@@ -0,0 +1,48 @@
+package eu.h2020.symbiote.enabler.messaging.model;
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import eu.h2020.symbiote.core.model.Observation;
+
+
+/**
+* @author PetarKrivic <petar.krivic@fer.hr>
+*/
+public class EnablerLogicDataAppearedMessage {
+
+	@JsonProperty("taskId")
+    private String taskId;
+	
+	@JsonProperty("timestamp")
+	private String timestamp;
+	
+	@JsonProperty("data")
+	private List<Observation> observations;
+	
+	public EnablerLogicDataAppearedMessage(){
+	}
+
+	public String getTaskId() {
+		return taskId;
+	}
+
+	public void setTaskId(String taskId) {
+		this.taskId = taskId;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public List<Observation> getObservations() {
+		return observations;
+	}
+
+	public void setObservations(List<Observation> observations) {
+		this.observations = observations;
+	}
+}
