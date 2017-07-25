e26fdde89906fac59eebdde68df217ebd019106c
Szymon Mueller <szymon.mueller@man.poznan.pl>
2017-07-25 15:14:47 +0200

    Adding MIM related info/updates to model classes

--- src/main/java/eu/h2020/symbiote/core/model/Platform.java
+++ src/main/java/eu/h2020/symbiote/core/model/Platform.java
@@ -2,84 +2,50 @@ package eu.h2020.symbiote.core.model;
 
 import org.springframework.data.annotation.Id;
 
+import java.util.List;
 
-public class Platform {
+
+public class Platform extends RDFInfo {
 
     @Id
-    private String platformId;
-    private String name;
-    private String description;
-    private String url;
-    private String informationModelId;
+    private String id;
+    private List<String> labels;
+    private List<String> comments;
+    private List<InterworkingService> interworkingServices;
 
     public Platform() {
 
     }
 
-    public String getPlatformId() {
-        return platformId;
+    public String getId() {
+        return id;
     }
 
-    public void setPlatformId(String platformId) {
-        this.platformId = platformId;
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public List<String> getLabels() {
+        return labels;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setLabels(List<String> labels) {
+        this.labels = labels;
     }
 
-    public String getDescription() {
-        return description;
+    public List<String> getComments() {
+        return comments;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setComments(List<String> comments) {
+        this.comments = comments;
     }
 
-    public String getUrl() {
-        return url;
+    public List<InterworkingService> getInterworkingServices() {
+        return interworkingServices;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getInformationModelId() {
-        return informationModelId;
-    }
-
-    public void setInformationModelId(String informationModelId) {
-        this.informationModelId = informationModelId;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Platform platform = (Platform) o;
-
-        if (platformId != null ? !platformId.equals(platform.platformId) : platform.platformId != null) return false;
-        if (name != null ? !name.equals(platform.name) : platform.name != null) return false;
-        if (description != null ? !description.equals(platform.description) : platform.description != null)
-            return false;
-        if (url != null ? !url.equals(platform.url) : platform.url != null) return false;
-        return informationModelId != null ? informationModelId.equals(platform.informationModelId) : platform
-                .informationModelId == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = platformId != null ? platformId.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (description != null ? description.hashCode() : 0);
-        result = 31 * result + (url != null ? url.hashCode() : 0);
-        result = 31 * result + (informationModelId != null ? informationModelId.hashCode() : 0);
-        return result;
+    public void setInterworkingServices(List<InterworkingService> interworkingServices) {
+        this.interworkingServices = interworkingServices;
     }
 }
