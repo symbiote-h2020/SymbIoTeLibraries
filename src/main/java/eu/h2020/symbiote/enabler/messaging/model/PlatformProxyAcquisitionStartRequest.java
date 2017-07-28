9c95d95364e63d263763c37f4354997976e8b872
Vasilis Glykantzis <vasgl@intracom-telecom.com>
2017-07-28 12:51:29 +0300

    Fixed previous commit

--- src/main/java/eu/h2020/symbiote/enabler/messaging/model/PlatformProxyAcquisitionStartRequest.java
+++ src/main/java/eu/h2020/symbiote/enabler/messaging/model/PlatformProxyAcquisitionStartRequest.java
@@ -1,8 +1,5 @@
 package eu.h2020.symbiote.enabler.messaging.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.List;
-
 
 public class PlatformProxyAcquisitionStartRequest extends PlatformProxyTaskInfo {
 
--- src/main/java/eu/h2020/symbiote/enabler/messaging/model/ResourcesUpdated.java
+++ src/main/java/eu/h2020/symbiote/enabler/messaging/model/ResourcesUpdated.java
@@ -7,6 +7,26 @@ import java.util.List;
 /**
  * Created by vasgl on 7/26/2017.
  */
-public class ResourcesUpdated extends PlatformProxyTaskInfo {
+public class ResourcesUpdated {
 
+    @JsonProperty("taskId")
+    private String taskId;
+
+    @JsonProperty("newResources")
+    private List<String> newResources;
+
+    public ResourcesUpdated() {
+        // empty constructor
+    }
+
+    public ResourcesUpdated(String taskId, List<String> newResources) {
+        this.taskId = taskId;
+        this.newResources = newResources;
+    }
+
+    public String getTaskId() { return taskId; }
+    public void setTaskId(String taskId) { this.taskId = taskId; }
+
+    public List<String> getNewResources() { return  newResources; }
+    public void setNewResources(List<String> newResources) { this.newResources = newResources; }
 }
