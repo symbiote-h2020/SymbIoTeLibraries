0b61a8f1c80c391adf2b4f5626b3a6f62b9b5d0c
Vasilis Glykantzis <vasgl@intracom-telecom.com>
2017-07-28 13:02:25 +0300

    Fixed

--- src/main/java/eu/h2020/symbiote/enabler/messaging/model/PlatformProxyTaskInfo.java
+++ src/main/java/eu/h2020/symbiote/enabler/messaging/model/PlatformProxyTaskInfo.java
@@ -2,32 +2,49 @@ package eu.h2020.symbiote.enabler.messaging.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
 
 public class PlatformProxyTaskInfo {
 
-    @JsonProperty("resourceId")
-    private String resourceId;
 
-    @JsonProperty("accessURL")
-    private String accessURL;
+    @JsonProperty("taskId")
+    private String taskId;
+
+    @JsonProperty("resources")
+    private List<PlatformProxyResourceInfo> resources;
+
+    @JsonProperty("enablerLogicName")
+    private String enablerLogicName;
+
+    // Subject to change to more human friendly format
+    @JsonProperty("queryInterval_ms")
+    private Integer queryInterval_ms;
 
 
     public PlatformProxyTaskInfo() {
     }
 
-    public String getResourceId() {
-        return resourceId;
+    public String getTaskId() {
+        return taskId;
+    }
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
     }
 
-    public void setResourceId(String resourceId) {
-        this.resourceId = resourceId;
+    public List<PlatformProxyResourceInfo> getResources() {
+        return resources;
+    }
+    public void setResources(List<PlatformProxyResourceInfo> resources) {
+        this.resources = resources;
     }
 
-    public String getAccessURL() {
-        return accessURL;
+    public Integer getQueryInterval_ms() {
+        return queryInterval_ms;
+    }
+    public void setQueryInterval_ms(Integer queryInterval_ms) {
+        this.queryInterval_ms = queryInterval_ms;
     }
 
-    public void setAccessURL(String accessURL) {
-        this.accessURL = accessURL;
-    }
+    public String getEnablerLogicName() { return enablerLogicName; }
+    public void setEnablerLogicName(String enablerLogicName) {this.enablerLogicName = enablerLogicName; }
 }
--- src/main/java/eu/h2020/symbiote/enabler/messaging/model/PlatformProxyUpdateRequest.java
+++ src/main/java/eu/h2020/symbiote/enabler/messaging/model/PlatformProxyUpdateRequest.java
@@ -1,6 +1,6 @@
 package eu.h2020.symbiote.enabler.messaging.model;
 
 
-public class PlatformProxyUpdateRequest extends PlatformProxyResourceInfo {
+public class PlatformProxyUpdateRequest extends PlatformProxyTaskInfo {
 
 }
