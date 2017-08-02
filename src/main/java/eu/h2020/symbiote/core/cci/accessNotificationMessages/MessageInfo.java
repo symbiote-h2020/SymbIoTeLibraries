f594eefcbadbd79c12b49d46e91eb876fd57c1f1
Vasilis Glykantzis <vasgl@intracom-telecom.com>
2017-08-02 13:27:38 +0300

    Added RAP Notification messages

---
+++ src/main/java/eu/h2020/symbiote/core/cci/accessNotificationMessages/MessageInfo.java
@@ -0,0 +1,31 @@
+package eu.h2020.symbiote.core.cci.accessNotificationMessages;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Luca Tomaselli <l.tomaselli@nextworks.it>
+ */
+public class MessageInfo {
+    @JsonProperty("symbIoTeId")
+    protected String symbIoTeId;
+    @JsonProperty("timestamp")
+    protected List<Date> timestamps;
+
+    public String getSymbIoTeId() {
+        return symbIoTeId;
+    }
+    public void setSymbIoTeId(String symbIoTeId) {
+        this.symbIoTeId = symbIoTeId;
+    }
+
+    public List<Date> getTimestamps() {
+        return timestamps;
+    }
+    public void setTimestamps(List<Date> timestamps) {
+        this.timestamps = timestamps;
+    }
+}
---
+++ src/main/java/eu/h2020/symbiote/core/cci/accessNotificationMessages/SuccessfulAccessMessageInfo.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.h2020.symbiote.core.cci.accessNotificationMessages;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Luca Tomaselli <l.tomaselli@nextworks.it>
+ */
+public class SuccessfulAccessMessageInfo extends MessageInfo {
+    public enum AccessType {
+        NORMAL,SUBSCRIPTION_START,SUBSCRIPTION_END
+    }
+    
+    @JsonProperty("accessType")
+    private String accessType;
+
+    public SuccessfulAccessMessageInfo() {
+        this.timestamps = new ArrayList<>();
+    }
+
+    public SuccessfulAccessMessageInfo(String symbIoTeId, List<Date> timestamps, String accessType) {
+        this.symbIoTeId = symbIoTeId;
+        this.timestamps = timestamps;
+        this.accessType = accessType;
+    }
+
+    public String getAccessType() {
+        return accessType;
+    }
+    public void setAccessType(String accessType) {
+        this.accessType = accessType;
+    }
+    
+}
