ba0545002fd9e0f036505e9bee58d91d6258e670
Mateusz Łukaszenko <mateuszl@man.poznan.pl>
2017-08-07 12:59:08 +0200

    parameters update

---
+++ src/main/java/eu/h2020/symbiote/cloud/model/data/parameter/Restriction.java
@@ -0,0 +1,11 @@
+package eu.h2020.symbiote.cloud.model.data.parameter;
+
+
+/**
+ * Represents CIM-defined Restriction class.
+ *
+ * Created by Mael on 28/03/2017.
+ */
+public abstract class Restriction {
+
+}
