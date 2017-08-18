e799b86b94f285373c5a6348be8373e905b0e41b
Szymon Mueller <szymon.mueller@man.poznan.pl>
2017-08-18 16:28:58 +0200

    Adding setter for name

--- src/main/java/eu/h2020/symbiote/core/model/resources/Parameter.java
+++ src/main/java/eu/h2020/symbiote/core/model/resources/Parameter.java
@@ -23,6 +23,10 @@ public class Parameter {
 
     public Parameter() {}
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
