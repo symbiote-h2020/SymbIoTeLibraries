bb74c158a256e983d64c8c1645c258ec188fb672
mkusek <mario.kusek@fer.hr>
2017-09-07 10:15:54 +0200

    Added Result class that is return message from calling service resource in cloud

---
+++ src/main/java/eu/h2020/symbiote/cloud/model/data/Result.java
@@ -0,0 +1,57 @@
+package eu.h2020.symbiote.cloud.model.data;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Represents return value from using service resource.
+ * 
+ * @author Mario Kušek <mario.kusek@fer.hr>
+ *
+ * @param <D> concrete result of service
+ */
+public class Result<D> {
+
+    @JsonIgnore
+    private boolean isArray;
+
+    @JsonIgnore
+    private String datatype;
+
+    @JsonProperty("value")
+    private D value;
+
+    
+    public Result() {
+    }
+    
+    public Result(boolean isArray, String datatype, D value) {
+        this.isArray = isArray;
+        this.datatype = datatype;
+        this.value = value;
+    }
+
+    public boolean isArray() {
+        return isArray;
+    }
+
+    public void setArray(boolean isArray) {
+        this.isArray = isArray;
+    }
+
+    public String getDatatype() {
+        return datatype;
+    }
+
+    public void setDatatype(String datatype) {
+        this.datatype = datatype;
+    }
+
+    public D getValue() {
+        return value;
+    }
+
+    public void setValue(D value) {
+        this.value = value;
+    }
+}
