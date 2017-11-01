aed301421d4eac6c1b20655a7d2886b3db6ab916
vthglyk <vasgl@intracom-telecom.com>
2017-11-01 12:10:21 +0200

    Added resourceDescriptions in ResourcesUpdated (SYM-701)

--- src/main/java/eu/h2020/symbiote/enabler/messaging/model/ResourcesUpdated.java
+++ src/main/java/eu/h2020/symbiote/enabler/messaging/model/ResourcesUpdated.java
@@ -1,6 +1,7 @@
 package eu.h2020.symbiote.enabler.messaging.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import eu.h2020.symbiote.core.ci.QueryResourceResult;
 
 import java.util.List;
 
@@ -15,13 +16,17 @@ public class ResourcesUpdated {
     @JsonProperty("newResources")
     private List<String> newResources;
 
+    @JsonProperty("resourceDescriptions")
+    private List<QueryResourceResult> resourceDescriptions;
+
     public ResourcesUpdated() {
         // empty constructor
     }
 
-    public ResourcesUpdated(String taskId, List<String> newResources) {
+    public ResourcesUpdated(String taskId, List<String> newResources, List<QueryResourceResult> resourceDescriptions) {
         this.taskId = taskId;
         this.newResources = newResources;
+        this.resourceDescriptions = resourceDescriptions;
     }
 
     public String getTaskId() { return taskId; }
@@ -29,4 +34,12 @@ public class ResourcesUpdated {
 
     public List<String> getNewResources() { return  newResources; }
     public void setNewResources(List<String> newResources) { this.newResources = newResources; }
+
+    public List<QueryResourceResult> getResourceDescriptions() {
+        return resourceDescriptions;
+    }
+
+    public void setResourceDescriptions(List<QueryResourceResult> resourceDescriptions) {
+        this.resourceDescriptions = resourceDescriptions;
+    }
 }
