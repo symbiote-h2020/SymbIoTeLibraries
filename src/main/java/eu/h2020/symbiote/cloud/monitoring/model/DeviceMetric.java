d0356d791dc85c7adc9ec63cf96e40e2c2ddcfbe
Jose Antonio Sánchez Murillo <jose.sanchezm@atos.net>
2018-01-08 15:26:06 +0100

    Lower spring boot version and figure out a way to determine which insertions have failed

--- src/main/java/eu/h2020/symbiote/cloud/monitoring/model/DeviceMetric.java
+++ src/main/java/eu/h2020/symbiote/cloud/monitoring/model/DeviceMetric.java
@@ -1,5 +1,7 @@
 package eu.h2020.symbiote.cloud.monitoring.model;
 
+import java.util.Objects;
+
 public class DeviceMetric extends Metric {
   
   
@@ -11,6 +13,17 @@ public class DeviceMetric extends Metric {
   
   public void setDeviceId(String deviceId) {
     this.deviceId = deviceId;
+  }
+  
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    DeviceMetric that = (DeviceMetric) o;
+    return Objects.equals(getDeviceId(), that.getDeviceId()) &&
+               Objects.equals(getTag(), that.getTag()) &&
+               Objects.equals(getDate(), that.getDate()) &&
+               Objects.equals(getValue(), that.getValue());
   }
   
 }
