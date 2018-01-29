0936247566754f9e0e326a6de58cf1328b310412
Christoph Ruggenthaler <christoph.ruggenthaler@ait.ac.at>
2018-01-29 10:01:13 +0100

    added informationModel to MIM [SYM-685]

--- src/main/java/eu/h2020/symbiote/model/mim/Federation.java
+++ src/main/java/eu/h2020/symbiote/model/mim/Federation.java
@@ -1,71 +1,82 @@
 package eu.h2020.symbiote.model.mim;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * @author RuggenthalerC
  *
- * Main object to represent one federation.
+ *         Main object to represent one federation.
  */
 public class Federation {
-    @Id
-    @JsonProperty("id")
-    private String id;
+	@Id
+	@JsonProperty("id")
+	private String id;
 
-    @JsonProperty("name")
-    private String name;
+	@JsonProperty("name")
+	private String name;
 
-    @JsonProperty("public")
-    private boolean isPublic = true;
+	@JsonProperty("public")
+	private boolean isPublic = true;
 
-    @JsonProperty("slaConstraints")
-    private List<QoSConstraint> slaConstraints;
+	@JsonProperty("informationModel")
+	private InformationModel informationModel;
 
-    @JsonProperty("members")
-    private List<FederationMember> members = new ArrayList<>();
+	@JsonProperty("slaConstraints")
+	private List<QoSConstraint> slaConstraints;
 
-    public String getId() {
-        return this.id;
-    }
+	@JsonProperty("members")
+	private List<FederationMember> members = new ArrayList<>();
 
-    public String getName() {
-        return this.name;
-    }
+	public String getId() {
+		return this.id;
+	}
 
-    public boolean isPublic() {
-        return this.isPublic;
-    }
+	public String getName() {
+		return this.name;
+	}
 
-    public List<QoSConstraint> getSlaConstraints() {
-        return this.slaConstraints;
-    }
+	public boolean isPublic() {
+		return this.isPublic;
+	}
 
-    public List<FederationMember> getMembers() {
-        return this.members;
-    }
+	public List<QoSConstraint> getSlaConstraints() {
+		return this.slaConstraints;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public List<FederationMember> getMembers() {
+		return this.members;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public void setPublic(boolean isPublic) {
-        this.isPublic = isPublic;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setSlaConstraints(List<QoSConstraint> slaConstraints) {
-        this.slaConstraints = slaConstraints;
-    }
+	public void setPublic(boolean isPublic) {
+		this.isPublic = isPublic;
+	}
 
-    public void setMembers(List<FederationMember> members) {
-        this.members = members;
-    }
+	public void setSlaConstraints(List<QoSConstraint> slaConstraints) {
+		this.slaConstraints = slaConstraints;
+	}
+
+	public void setMembers(List<FederationMember> members) {
+		this.members = members;
+	}
+
+	public InformationModel getInformationModel() {
+		return this.informationModel;
+	}
+
+	public void setInformationModel(InformationModel informationModel) {
+		this.informationModel = informationModel;
+	}
 }
