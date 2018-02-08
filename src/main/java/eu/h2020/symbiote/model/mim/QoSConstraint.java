7b06f5bd3e74dd8893a2a8b8e7c06b7e4f3de7a5
vthglyk <vasgl@intracom-telecom.com>
2018-02-08 15:30:14 +0200

    Added validation on QoSConstraint class

--- src/main/java/eu/h2020/symbiote/model/mim/QoSConstraint.java
+++ src/main/java/eu/h2020/symbiote/model/mim/QoSConstraint.java
@@ -2,18 +2,26 @@ package eu.h2020.symbiote.model.mim;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 public class QoSConstraint {
   
   @JsonProperty("metric")
+  @NotNull
   private QoSMetric metric;
   
   @JsonProperty("comparator")
+  @NotNull
   private Comparator comparator;
   
   @JsonProperty("threshold")
-  private double threshold;
+  @NotNull
+  @Valid
+  private Double threshold;
   
   @JsonProperty("duration")
+  @Valid
   private Integer duration;
   
   @JsonProperty("resourceType")
@@ -22,7 +30,6 @@ public class QoSConstraint {
   public QoSMetric getMetric() {
     return metric;
   }
-  
   public void setMetric(QoSMetric metric) {
     this.metric = metric;
   }
@@ -30,23 +37,20 @@ public class QoSConstraint {
   public Comparator getComparator() {
     return comparator;
   }
-  
   public void setComparator(Comparator comparator) {
     this.comparator = comparator;
   }
   
-  public double getThreshold() {
+  public Double getThreshold() {
     return threshold;
   }
-  
-  public void setThreshold(double threshold) {
+  public void setThreshold(Double threshold) {
     this.threshold = threshold;
   }
   
   public Integer getDuration() {
     return duration;
   }
-  
   public void setDuration(Integer duration) {
     this.duration = duration;
   }
@@ -54,7 +58,6 @@ public class QoSConstraint {
   public String getResourceType() {
     return resourceType;
   }
-  
   public void setResourceType(String resourceType) {
     this.resourceType = resourceType;
   }
