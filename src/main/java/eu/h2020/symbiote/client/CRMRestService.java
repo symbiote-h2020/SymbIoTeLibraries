22e57f5ea584e8a50f5b68a83611b249f43c6ba1
Jose Antonio Sanchez <jose.sanchezm@atos.net>
2018-03-08 16:00:17 +0100

    Fix content type for CRAM and CRM clients

--- src/main/java/eu/h2020/symbiote/client/CRMRestService.java
+++ src/main/java/eu/h2020/symbiote/client/CRMRestService.java
@@ -1,15 +1,15 @@
 package eu.h2020.symbiote.client;
 
 
-
-
 import eu.h2020.symbiote.cloud.monitoring.model.CloudMonitoringPlatform;
+import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
 
 public interface CRMRestService {
 	
 	@RequestLine("POST " + ClientConstants.PUBLISH_MONITORING_DATA)
+	@Headers("Content-Type: application/json")
     public String doPost2Crm(
     		@Param("platformId") String platformId, 
     		CloudMonitoringPlatform platform);
