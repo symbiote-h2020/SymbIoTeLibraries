cbdb9f8cd145ed27d47254f27598d9074c9c7fe0
Szymon Mueller <szymon.mueller@man.poznan.pl>
2018-03-25 23:45:31 +0200

    Adding missing equals/hash/toString of capability

--- src/main/java/eu/h2020/symbiote/model/cim/Capability.java
+++ src/main/java/eu/h2020/symbiote/model/cim/Capability.java
@@ -43,5 +43,34 @@ public class Capability {
     public void setName(String name) {
         this.name = name;
     }
-    
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Capability that = (Capability) o;
+
+        if (parameters != null ? !parameters.equals(that.parameters) : that.parameters != null) return false;
+        if (effects != null ? !effects.equals(that.effects) : that.effects != null) return false;
+        return name != null ? name.equals(that.name) : that.name == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = parameters != null ? parameters.hashCode() : 0;
+        result = 31 * result + (effects != null ? effects.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Capability{" +
+                "parameters=" + parameters +
+                ", effects=" + effects +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
