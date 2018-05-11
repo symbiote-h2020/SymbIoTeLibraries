f92120e768444a477ebf7d53c5d2fd87595098ee
Szymon Mueller <szymon.mueller@man.poznan.pl>
2018-05-11 11:54:15 +0200

    Adding missing documentation and equals/hashcodes/tostrings of some enabler beans.

--- src/main/java/eu/h2020/symbiote/enabler/messaging/model/PlatformProxyAcquisitionStartRequestResponse.java
+++ src/main/java/eu/h2020/symbiote/enabler/messaging/model/PlatformProxyAcquisitionStartRequestResponse.java
@@ -4,6 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 
+/**
+ * Response to acquisition start request for Plaform Proxy. Returns human readable status of the acquisition request
+ * and taskId of the started acquisition.
+ *
+ */
 public class PlatformProxyAcquisitionStartRequestResponse {
 
     @JsonProperty("status")
@@ -32,4 +37,30 @@ public class PlatformProxyAcquisitionStartRequestResponse {
         this.taskId = taskId;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PlatformProxyAcquisitionStartRequestResponse that = (PlatformProxyAcquisitionStartRequestResponse) o;
+
+        if (status != null ? !status.equals(that.status) : that.status != null) return false;
+        return taskId != null ? taskId.equals(that.taskId) : that.taskId == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = status != null ? status.hashCode() : 0;
+        result = 31 * result + (taskId != null ? taskId.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "PlatformProxyAcquisitionStartRequestResponse{" +
+                "status='" + status + '\'' +
+                ", taskId='" + taskId + '\'' +
+                '}';
+    }
 }
