6331afddc6870912f633b44e6f75463325d194c4
Szymon Mueller <szymon.mueller@man.poznan.pl>
2018-05-11 11:54:15 +0200

    Adding missing documentation and equals/hashcodes/tostrings of some enabler beans.

--- src/main/java/eu/h2020/symbiote/enabler/messaging/model/PlatformProxyResourceInfo.java
+++ src/main/java/eu/h2020/symbiote/enabler/messaging/model/PlatformProxyResourceInfo.java
@@ -29,4 +29,31 @@ public class PlatformProxyResourceInfo {
     public void setAccessURL(String accessURL) {
         this.accessURL = accessURL;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PlatformProxyResourceInfo that = (PlatformProxyResourceInfo) o;
+
+        if (resourceId != null ? !resourceId.equals(that.resourceId) : that.resourceId != null) return false;
+        return accessURL != null ? accessURL.equals(that.accessURL) : that.accessURL == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = resourceId != null ? resourceId.hashCode() : 0;
+        result = 31 * result + (accessURL != null ? accessURL.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "PlatformProxyResourceInfo{" +
+                "resourceId='" + resourceId + '\'' +
+                ", accessURL='" + accessURL + '\'' +
+                '}';
+    }
 }
--- src/main/java/eu/h2020/symbiote/enabler/messaging/model/PlatformProxyTaskInfo.java
+++ src/main/java/eu/h2020/symbiote/enabler/messaging/model/PlatformProxyTaskInfo.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
+/**
+ * Class describing Platform Proxy task. Contains id of the task, list of resources task is attempting to connect to,
+ * name of the Enabler Logic that requested the task and task recurrence interval.
+ */
 public class PlatformProxyTaskInfo {
 
 
@@ -47,4 +51,38 @@ public class PlatformProxyTaskInfo {
 
     public String getEnablerLogicName() { return enablerLogicName; }
     public void setEnablerLogicName(String enablerLogicName) {this.enablerLogicName = enablerLogicName; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PlatformProxyTaskInfo that = (PlatformProxyTaskInfo) o;
+
+        if (taskId != null ? !taskId.equals(that.taskId) : that.taskId != null) return false;
+        if (resources != null ? !resources.equals(that.resources) : that.resources != null) return false;
+        if (enablerLogicName != null ? !enablerLogicName.equals(that.enablerLogicName) : that.enablerLogicName != null)
+            return false;
+        return queryInterval_ms != null ? queryInterval_ms.equals(that.queryInterval_ms) : that.queryInterval_ms == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = taskId != null ? taskId.hashCode() : 0;
+        result = 31 * result + (resources != null ? resources.hashCode() : 0);
+        result = 31 * result + (enablerLogicName != null ? enablerLogicName.hashCode() : 0);
+        result = 31 * result + (queryInterval_ms != null ? queryInterval_ms.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "PlatformProxyTaskInfo{" +
+                "taskId='" + taskId + '\'' +
+                ", resources=" + resources +
+                ", enablerLogicName='" + enablerLogicName + '\'' +
+                ", queryInterval_ms=" + queryInterval_ms +
+                '}';
+    }
 }
