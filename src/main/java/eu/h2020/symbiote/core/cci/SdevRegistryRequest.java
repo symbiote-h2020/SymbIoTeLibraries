f7f4578c511e5d33a40f40bd6b581ad5d3d96f95
Szymon Mueller <szymon.mueller@man.poznan.pl>
2018-05-29 10:24:50 +0200

    Renaming SspSDEVInfo to SspRegInfo

--- src/main/java/eu/h2020/symbiote/core/cci/SdevRegistryRequest.java
+++ src/main/java/eu/h2020/symbiote/core/cci/SdevRegistryRequest.java
@@ -1,15 +1,14 @@
 package eu.h2020.symbiote.core.cci;
 
-import eu.h2020.symbiote.cloud.model.ssp.SspSDEVInfo;
-import eu.h2020.symbiote.security.communication.payloads.SecurityRequest;
+import eu.h2020.symbiote.cloud.model.ssp.SspRegInfo;
 
 /**
  * Class representing SDEV registry request.
  *
  * Created by mateuszl on 25.05.2018.
  */
-public class SdevRegistryRequest extends AbstractRequest<SspSDEVInfo> {
-    public SdevRegistryRequest(SspSDEVInfo body) {
+public class SdevRegistryRequest extends AbstractRequest<SspRegInfo> {
+    public SdevRegistryRequest(SspRegInfo body) {
         super(body);
     }
 
--- src/main/java/eu/h2020/symbiote/core/cci/SdevRegistryResponse.java
+++ src/main/java/eu/h2020/symbiote/core/cci/SdevRegistryResponse.java
@@ -1,16 +1,16 @@
 package eu.h2020.symbiote.core.cci;
 
-import eu.h2020.symbiote.cloud.model.ssp.SspSDEVInfo;
+import eu.h2020.symbiote.cloud.model.ssp.SspRegInfo;
 
 /**
  * Created by mateuszl on 25.05.2018.
  */
-public class SdevRegistryResponse extends AbstractResponse<SspSDEVInfo> {
+public class SdevRegistryResponse extends AbstractResponse<SspRegInfo> {
     public SdevRegistryResponse() {
         // Needed for Jackson serialization
     }
 
-    public SdevRegistryResponse(int status, String message, SspSDEVInfo body) {
+    public SdevRegistryResponse(int status, String message, SspRegInfo body) {
         super(status, message, body);
     }
 }
