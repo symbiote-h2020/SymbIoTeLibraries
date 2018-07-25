686580626a02bbdcafecd8d62e1b6ef259355453
vthglyk <vasgl@intracom-telecom.com>
2018-07-25 12:29:49 +0300

    Proposal for Subscriptions

--- src/main/java/eu/h2020/symbiote/cloud/model/internal/Subscription.java
+++ src/main/java/eu/h2020/symbiote/cloud/model/internal/Subscription.java
@@ -1,5 +1,6 @@
 package eu.h2020.symbiote.cloud.model.internal;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,84 +8,96 @@ import java.util.Map;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import org.springframework.data.annotation.Id;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.annotation.PersistenceConstructor;
 
 /**
  * @author Petar Krivic 16/05/2018.
- * 
+ *
  * Subscription model class.
  */
 public class Subscription {
-	
-	//federationMemberID
-	@Id
-	@JsonProperty("platformId")
-	private String platformId;
-	
-	//flags defining wanted resourceTypes
-	@JsonProperty("resourceType")
-	private Map<String, Boolean> resourceType;
 
-	//names of preferred device locations (OPTIONAL)
-	@JsonProperty("locations")
-	private List<String> locations;
-	
-	//names of preferred observedProperties for sensors (OPTIONAL)
-	@JsonProperty("observedProperties")
-	private List<String> observedProperties;
-	
-	//names of preferred capabilities for actuators (OPTIONAL)
-	@JsonProperty("capabilities")
-	private List<String> capabilities;
-	
-	public Subscription(){
-		this.resourceType = new HashMap<>();
-		this.resourceType.put("service", true);
-		this.resourceType.put("device", true);
-		this.resourceType.put("actuator", true);
-		this.resourceType.put("sensor", true);
-	}
+    @Id
+    private String id;
 
-	public String getPlatformId() {
-		return platformId;
-	}
+    //federationMemberID
+    private String platformId;
 
-	public void setPlatformId(String platformId) {
-		this.platformId = platformId;
-	}
+    private String federationId;
 
-	public Map<String, Boolean> getResourceType() {
-		return resourceType;
-	}
+    //flags defining wanted resourceTypes
+    private Map<String, Boolean> resourceType;
 
-	public void setResourceType(Map<String, Boolean> resourceType) {
-		this.resourceType = resourceType;
-	}
+    //names of preferred device locations (OPTIONAL)
+    private List<String> locations;
 
-	public List<String> getLocations() {
-		return locations;
-	}
+    //names of preferred observedProperties for sensors (OPTIONAL)
+    private List<String> observedProperties;
 
-	public void setLocations(List<String> locations) {
-		this.locations = locations;
-	}
+    //names of preferred capabilities for actuators (OPTIONAL)
+    private List<String> capabilities;
 
-	public List<String> getObservedProperties() {
-		return observedProperties;
-	}
+    public Subscription(String platformId, String federationId) {
+        setId(platformId, federationId);
+        this.platformId = platformId;
+        this.federationId = federationId;
+        this.resourceType = new HashMap<>();
+        this.resourceType.put("service", true);
+        this.resourceType.put("device", true);
+        this.resourceType.put("actuator", true);
+        this.resourceType.put("sensor", true);
+        this.locations = new ArrayList<>();
+        this.observedProperties = new ArrayList<>();
+        this.capabilities = new ArrayList<>();
+    }
 
-	public void setObservedProperties(List<String> observedProperties) {
-		this.observedProperties = observedProperties;
-	}
+    @PersistenceConstructor
+    @JsonCreator
+    public Subscription(@JsonProperty("platformId") String platformId,
+                        @JsonProperty("federationId") String federationId,
+                        @JsonProperty("resourceType") Map<String, Boolean> resourceType,
+                        @JsonProperty("locations") List<String> locations,
+                        @JsonProperty("observedProperties") List<String> observedProperties,
+                        @JsonProperty("capabilities") List<String> capabilities) {
+        setId(platformId, federationId);
+        this.platformId = platformId;
+        this.federationId = federationId;
+        this.resourceType = resourceType;
+        this.locations = locations;
+        this.observedProperties = observedProperties;
+        this.capabilities = capabilities;
+    }
 
-	public List<String> getCapabilities() {
-		return capabilities;
-	}
+    public String getId() { return id; }
 
-	public void setCapabilities(List<String> capabilities) {
-		this.capabilities = capabilities;
-	}
-	
+    public void setId(String platformId, String federationId) { this.id = platformId + "@" + federationId; }
+
+    public String getFederationId() { return federationId; }
+
+    public void setFederationId(String federationId) { this.federationId = federationId; }
+
+    public String getPlatformId() { return platformId; }
+
+    public void setPlatformId(String platformId) { this.platformId = platformId; }
+
+    public Map<String, Boolean> getResourceType() { return resourceType; }
+
+    public void setResourceType(Map<String, Boolean> resourceType) { this.resourceType = resourceType; }
+
+    public List<String> getLocations() { return locations; }
+
+    public void setLocations(List<String> locations) { this.locations = locations; }
+
+    public List<String> getObservedProperties() { return observedProperties; }
+
+    public void setObservedProperties(List<String> observedProperties) { this.observedProperties = observedProperties; }
+
+    public List<String> getCapabilities() { return capabilities; }
+
+    public void setCapabilities(List<String> capabilities) { this.capabilities = capabilities; }
+
 }
